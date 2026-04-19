fbb31c694eb94b8144b35a234a8cfdbc784795cd
hebeled <akinbaltam@outlook.com>
2026-04-19 06:58:21 +0100

    Add Square class

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,37 @@
-public class Square {
+public class Square extends Shape {
+    private int side;
+
+    public Square(Coordinates coord, int side) {
+        super(4, coord);
+        this.side = side;
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        position.scale(factor, sign);
+
+        if (sign) {
+            this.side *= factor;
+        } else {
+            this.side /= factor;
+        }
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public String display() {
+        return "Shape: Square\nPosition: " + position.display() +
+                "\nSide: " + side +
+                "\nArea: " + String.format("%.2f", getArea()) +
+                "\nPerimeter: " + String.format("%.2f", getPerimeter());
+    }
 }
