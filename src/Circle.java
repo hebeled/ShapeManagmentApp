31cfdde74bc160cb3ca3c28c3c5f1bdf13adac9e
hebeled <akinbaltam@outlook.com>
2026-04-19 06:54:49 +0100

    Add Circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,37 @@
-public class Circle {
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(Coordinates coord, double radius) {
+        super(0, coord);
+        this.radius = radius;
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        position.scale(factor, sign);
+
+        if (sign) {
+            this.radius *= factor;
+        } else {
+            this.radius /= factor;
+        }
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String display() {
+        return "Shape: Circle\nPosition: " + position.display() +
+                "\nRadius: " + radius +
+                "\nArea: " + String.format("%.2f", getArea()) +
+                "\nPerimeter: " + String.format("%.2f", getPerimeter());
+    }
 }
