343061c904883e3ff16052c95642333ca4a435c3
hebeled <akinbaltam@outlook.com>
2026-04-19 23:21:51 +0100

    Add comments to Coordinates class

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -2,30 +2,36 @@ public class Coordinates {
     private int x;
     private int y;
 
+    // Constructor to create a coordinate with x and y values
     public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    // Returns the x value of the coordinate
     public int getX() {
         return x;
     }
 
+    // Returns the y value of the coordinate
     public int getY() {
         return y;
     }
 
+    // Calculates the distance between this coordinate and another coordinate
     public double distance(Coordinates p) {
         int distX = this.x - p.getX();
         int distY = this.y - p.getY();
         return Math.sqrt(distX * distX + distY * distY);
     }
 
+    // Moves the coordinate by dx and dy
     public void translate(int dx, int dy) {
         this.x += dx;
         this.y += dy;
     }
 
+    // Scales the coordinate by multiplying or dividing with the factor
     public void scale(int factor, boolean sign) {
         if (sign) {
             this.x *= factor;
@@ -36,6 +42,7 @@ public class Coordinates {
         }
     }
 
+    // Returns the coordinate as a string
     public String display() {
         return "X = " + x + ", Y = " + y;
     }
