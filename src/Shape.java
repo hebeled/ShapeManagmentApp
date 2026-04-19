a89e70413306d5fc64f71a289b92562394a4f0e6
hebeled <akinbaltam@outlook.com>
2026-04-19 06:53:00 +0100

    Add abstract Shape class

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,33 @@
-public class Shape {
+public abstract class Shape {
+    protected Coordinates position;
+    protected int sides;
+
+    public Shape(int noOfSides, Coordinates coord) {
+        this.position = coord;
+        this.sides = noOfSides;
+    }
+
+    public Coordinates getCoordinates() {
+        return position;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+
+    public void setCoordinates(Coordinates newCoord) {
+        this.position = newCoord;
+    }
+
+    public void translate(int dx, int dy) {
+        position.translate(dx, dy);
+    }
+
+    public abstract void scale(int factor, boolean sign);
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+
+    public abstract String display();
 }
