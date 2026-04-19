0cd37aeb6f1ef9d64d9a16fedea13f5aec6645bc
hebeled <akinbaltam@outlook.com>
2026-04-19 06:47:14 +0100

    Add Coordinates class

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,2 +1,42 @@
 public class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(Coordinates p) {
+        int distX = this.x - p.getX();
+        int distY = this.y - p.getY();
+        return Math.sqrt(distX * distX + distY * distY);
+    }
+
+    public void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    public void scale(int factor, boolean sign) {
+        if (sign) {
+            this.x *= factor;
+            this.y *= factor;
+        } else {
+            this.x /= factor;
+            this.y /= factor;
+        }
+    }
+
+    public String display() {
+        return "X = " + x + ", Y = " + y;
+    }
 }
