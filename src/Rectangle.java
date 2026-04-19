9435b6b40a7911a05faaad787ddd5bc80a79c7ce
hebeled <akinbaltam@outlook.com>
2026-04-19 06:57:35 +0100

    Add Rectangle class

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,42 @@
-public class Rectangle {
+public class Rectangle extends Shape {
+    private int width;
+    private int length;
+
+    public Rectangle(Coordinates coord, int width, int length) {
+        super(4, coord);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public void scale(int factor, boolean sign) {
+        position.scale(factor, sign);
+
+        if (sign) {
+            this.width *= factor;
+            this.length *= factor;
+        } else {
+            this.width /= factor;
+            this.length /= factor;
+        }
+    }
+
+    @Override
+    public double getArea() {
+        return width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String display() {
+        return "Shape: Rectangle\nPosition: " + position.display() +
+                "\nWidth: " + width +
+                "\nLength: " + length +
+                "\nArea: " + String.format("%.2f", getArea()) +
+                "\nPerimeter: " + String.format("%.2f", getPerimeter());
+    }
 }
